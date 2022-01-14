5d00022f560ddf07297d22314182b6093862e656
Billy Bui <billybui768@gmail.com>
2022-01-14 00:15:02 -0800

    Added image getting logic

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
+const database = require("./database");
 const fs = require("fs");
 
-const upload = multer({ dest: "images/" });
-
 const app = express();
+// database.test();
+const upload = multer({ dest: "images/" });
 
 app.get("/images/:imageName", (req, res) => {
   const imageName = req.params.imageName;
@@ -12,12 +14,19 @@ app.get("/images/:imageName", (req, res) => {
   readStream.pipe(res);
 });
 
-app.post("/api/images", upload.single("image"), (req, res) => {
+app.post("/api/images", upload.single("image"), async (req, res) => {
   const imagePath = req.file.path;
   const description = req.body.description;
-  console.log(description, imagePath);
 
-  res.send({ description, imagePath });
+  const image = await database.addImage(imagePath, description);
+  res.send({ image });
 });
 
+app.get("/api/images", async (req, res) => {
+  const images = await database.getImages();
+
+  res.send({ images });
+});
+
+app.get("/api/images");
 app.listen(8080, () => console.log("listening on port 8080"));
